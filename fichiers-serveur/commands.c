26d13888a58b904afaed90f374bad99d3ed9e062
TRICOT-Hugo <hugo.tricot@insa-lyon.fr>
2022-10-28 10:47:42 +0200

    wip: core dumped in command parser

--- fichiers-serveur/commands.c
+++ fichiers-serveur/commands.c
@@ -11,26 +11,32 @@
 
 char **read_command(const char *command)
 {
-    char ** res = (char**) malloc(sizeof(char*) * (MAX_ARG + 1));
-    res[0] = (char*) malloc(sizeof(char)*3);
+    printf("Allocating res\r\n");;
+    char **res = (char**) malloc(sizeof(char*) * (MAX_ARG + 1));
+    res[0] = (char*) malloc(sizeof(char)*10);
     for(int i = 0; i < MAX_ARG; i++)
     {
+        printf("Allocating argument number %d\r\n", i+1);
         res[i+1] = (char * restrict) malloc(sizeof(char)*BUF_SIZE);
+        res[i+1] = "\0";
+        printf("Argument number %d\r\n", i+1);
     }
-    const char* restrict delimiter = " "; 
+    printf("Allocating complete\r\n");
+    const char* restrict delimiter = " ";
 
     char commandCopy[strlen(command) + 1];
-    strcpy(commandCopy, command);
-
+    strcat(commandCopy, command);
+    printf("commandCopy dealt with\r\n");
     char *temp = strtok(commandCopy, delimiter);
-
-    if(!strncmp("/register", command, 8) || !strncmp("/w", command, 2))
+    printf("obtained first temp : %s\r\n", temp);
+    if(!strncmp("/register", command, 8) || !strncmp("/r", command, 2))
     {
         /* /register [id] [password] */
-        strcpy(res[0], "0");
+        strcat(res[0], "0");
 
+        temp = strtok(NULL, delimiter);
         strcpy(res[1], temp);
-        temp = strtok(temp, delimiter);
+        temp = strtok(NULL, delimiter);
         strcpy(res[2], temp);
         
         printf("commande /register reçue avec comme paramètre : %s et %s\r\n", res[1], res[2]);
@@ -38,25 +44,30 @@ char **read_command(const char *command)
     else if (!strncmp("/join", command, 5))       // Checks only the first 5 characters of command (so it doesn't take the argument)
     {
         /* /join [chanel] */
-        strcpy(res[0], "1");
-
+        strcat(res[0], "1");
+        printf("Dealt with command\r\n");
+        temp = strtok(NULL, delimiter);
+        printf("before dealing with arg1\r\n");
+        if(temp == NULL) printf("arg1 is null");
         strcpy(res[1], temp);                             // On suppose que le message envoyé est bien /join
         printf("commande /join reçue avec comme paramètre : %s\r\n", res[1]);
     }
     else if(!strncmp("/leave", command, 6))
     {
-        strcpy(res[0], "2");
+        strcat(res[0], "2");
 
+        temp = strtok(NULL, delimiter);
         strcpy(res[1], temp);
         // Commande de leave
         printf("commande /leave reçue avec comme paramètre : %s\r\n", res[1]);
     }
     else if(!strncmp("/whisper", command, 8) || !strncmp("/w", command, 2))
     {
-        strcpy(res[0], "3");
+        strcat(res[0], "3");
 
+        temp = strtok(NULL, delimiter);
         strcpy(res[1], temp);
-        temp = strtok(temp, delimiter);
+        temp = strtok(NULL, delimiter);
         strcpy(res[2], temp);
         // Commande de whisper
         printf("commande /whisper reçue avec comme paramètre : %s et %s\r\n", res[1], res[2]);
