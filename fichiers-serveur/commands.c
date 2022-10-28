22a74909b1e3456bc7ce839e06e367bf7aa68a24
Omi <thibautf69220@gmail.com>
2022-10-28 08:04:12 +0200

    fix(commands): Fixed slash orientation

--- fichiers-serveur/commands.c
+++ fichiers-serveur/commands.c
@@ -19,25 +19,25 @@ static int read_command(const char *command)
     char *temp = strtok(commandCopy, delimiter);
 
     /* A vérifier : command est interprêté sans prendre en compte l'espacement du \ , */
-    if (!strncmp("\\join", command, 5))       // Checks only the first 5 characters of command (so it doesn't take the argument)
+    if (!strncmp("/join", command, 5))       // Checks only the first 5 characters of command (so it doesn't take the argument)
     {
-        strcpy(arg1, temp);                             // On suppose que le message envoyé est bien \join et pas \\join.
+        strcpy(arg1, temp);                             // On suppose que le message envoyé est bien \join et pas /join.
         // Commande de join
-        printf("commande \\join reçue avec comme paramètre : %s\r\n", arg1);
+        printf("commande /join reçue avec comme paramètre : %s\r\n", arg1);
     }
-    else if(!strncmp("\\leave", command, 6))
+    else if(!strncmp("/leave", command, 6))
     {
         strcpy(arg1, temp);
         // Commande de leave
-        printf("commande \\leave reçue avec comme paramètre : %s\r\n", arg1);
+        printf("commande /leave reçue avec comme paramètre : %s\r\n", arg1);
     }
-    else if(!strncmp("\\whisper", command, 8) || !strncmp("\\w", command, 2))
+    else if(!strncmp("/whisper", command, 8) || !strncmp("/w", command, 2))
     {
         strcpy(arg1, temp);
         temp = strtok(temp, delimiter);
         strcpy(arg2, temp);
         // Commande de whisper
-        printf("commande \\whisper reçue avec comme paramètre : %s et %s\r\n", arg1, arg2);
+        printf("commande /whisper reçue avec comme paramètre : %s et %s\r\n", arg1, arg2);
     }
     else {
         printf("Commande non reconnue\r\n");
